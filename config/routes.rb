45773b8ba6d10094f4b6b238e091b6800c3addd7
Asato TAKAHASHI <asato730@gmail.com>
2020-03-09 19:36:55 +0900

    vol.8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
+  get   '/sign_up',  to: 'users#new',    as: :sign_up
+  post  '/sign_up',  to: 'users#create', as: :create_user
   resources :users, only: [:show]
 end
